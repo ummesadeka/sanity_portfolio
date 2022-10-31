d640dc2a00c27e659ab7114e61e1a84d7442829a
Sadeka <sadekaahmed53@gmail.com>
2022-10-31 22:50:57 +0600

    Sanity setup

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -3,31 +3,17 @@ import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
-import { images } from '../../constants';
-// import { urlFor, client } from '../../client';
-const abouts = [
-  {
-    title: 'Web Development',
-    description: 'I am a good Web Developer',
-    imgUrl: images.about01,
-  },
-  {
-    title: 'Web Design',
-    description: 'I am a good Web Developer',
-    imgUrl: images.about02,
-  },
-  {
-    title: 'Web Animations',
-    description: 'I am a good Web Developer',
-    imgUrl: images.about03,
-  },
-  {
-    title: 'Full Stack',
-    description: 'I am a good Web Developer',
-    imgUrl: images.about04,
-  },
-];
+import { urlFor, client } from '../../client';
+
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts]';
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
   return (
     <>
       <h2 className="head-text">
