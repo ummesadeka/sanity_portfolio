58717e3b27e3449501c6ebc2af050b65d4b2ecde
Sadeka <sadekaahmed53@gmail.com>
2022-10-31 16:13:54 +0600

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
-// import { Navbar } from './components';
+import { Navbar } from './components';
 import './App.scss';
 
 const App = () => (
   <div className="app">
-    {/* <Navbar /> */}
+    <Navbar />
     <Header />
     <About />
     <Work />
